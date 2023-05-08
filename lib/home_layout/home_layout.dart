352ad76a74bffc33bb84002676c6ebe5a2976e2f
Mohamed Hamouda <mohamed.hamouda7897@gmail.com>
2023-05-08 20:25:22 +0300

    finish home layout

--- lib/home_layout/home_layout.dart
+++ lib/home_layout/home_layout.dart
@@ -1,17 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:todo_c8_monday/screens/settings_screen.dart';
+import 'package:todo_c8_monday/screens/tasks_screen.dart';
 
-class HomeLayout extends StatelessWidget {
+class HomeLayout extends StatefulWidget {
   static const String routeName = "HomeLayout";
+
+  @override
+  State<HomeLayout> createState() => _HomeLayoutState();
+}
+
+class _HomeLayoutState extends State<HomeLayout> {
+  int index = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBody: true,
       appBar: AppBar(
         title: Text(
           "ToDo App",
           style: Theme.of(context).textTheme.bodyLarge,
         ),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(
+          Icons.add,
+          size: 30,
+        ),
+        shape: StadiumBorder(
+          side: BorderSide(color: Colors.white, width: 3),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        notchMargin: 8,
+        shape: CircularNotchedRectangle(),
+        child: BottomNavigationBar(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          currentIndex: index,
+          onTap: (value) {
+            index = value;
+            setState(() {});
+          },
+          iconSize: 30,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.list), label: ""),
+            BottomNavigationBarItem(icon: Icon(Icons.settings), label: ""),
+          ],
+        ),
+      ),
+      body: tabs[index],
     );
   }
+
+  List<Widget> tabs = [TasksScreen(), SettingsScreen()];
 }
